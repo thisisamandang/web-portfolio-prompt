49999eec10cc2e2f9e28855944aee3d9fe07b7d6
thisisamandang <thisisamandang@gmail.com>
2024-01-13 12:27:33 +0530

    refactor

--- src/app/components/promptInput.tsx
+++ src/app/components/promptInput.tsx
@@ -22,15 +22,15 @@ const PromptInput = (props: Props) => {
       <div className="text-gray-600 absolute transform top-1/2 md:left-8 left-4 -translate-y-1/2 opacity-90">
         {props.inputValue ? null : (
           <>
-            <span>Tip: Press </span>{" "}
+            <span>Tip: Press </span>
             <span className=" border-[1px] border-gray-600 text-xs w-5 h-2 rounded-sm mx-1 px-1.5 md:py-0.5 py-1">
               /
-            </span>{" "}
+            </span>
             <span>for Menu</span>
           </>
         )}
       </div>
-      <form className="w-full h-16 gradient-border text-white relative">
+      <form className="w-full h-16 gradient-border text-white relative" >
         <input
           type="text"
           className="outline-none transform absolute bg-transparent text-black brightness-150 top-1/2 md:left-4 -translate-y-1/2 w-[90%] max-lg:w-[80%]"
